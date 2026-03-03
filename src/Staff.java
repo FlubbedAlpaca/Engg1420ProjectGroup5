642753ad4d12d36e5a6ab14c346659cb2109567e
Lucas <lucas.singh06@outlook.com>
2026-03-02 19:55:32 -0500

    rebase merge attempt

--- src/Staff.java
+++ src/Staff.java
@@ -1,11 +1,12 @@
-public class Staff extends User{
+public class Staff extends User {
     public Staff(String name, String userID, String email) {
         super(name, userID, email);
     }
 
     @Override
-    public int getLimit(){
+    public int getLimit() {
         return 5;
+    }
 }
 
 
