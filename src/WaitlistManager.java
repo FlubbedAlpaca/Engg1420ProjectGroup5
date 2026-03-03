dc970393ed11d7b1d1b667c355fe5e19fd7f4c89
Lucas <lucas.singh06@outlook.com>
2026-03-03 01:23:04 -0500

    Beginning of waitlistmanager. covers the beginning for the waitlist so adds users for waitlists

--- src/WaitlistManager.java
+++ src/WaitlistManager.java
@@ -14,20 +14,17 @@ public class WaitlistManager {
         int index = eventIDlist.indexOf(eventId);
 
         if (index == -1) {
-            // Event not in list yet so create new waitlist
             eventIDlist.add(eventId);
             ArrayList<User> newWaitlist = new ArrayList<User>();
             newWaitlist.add(user);
             allWaitlists.add(newWaitlist);
         } else {
-            // Event exists so add user to its waitlist
             ArrayList<User> existingWaitlist = allWaitlists.get(index);
             existingWaitlist.add(user);
         }
         System.out.println("User '" + user.getname() + "' added to waitlist for event " + eventId);
     }
 
-    // Check if an event has people waiting
     public boolean hasWaitlist(String eventId) {
         int index = eventIDlist.indexOf(eventId);
         if (index == -1) {
@@ -36,7 +33,7 @@ public class WaitlistManager {
         ArrayList<User> waitlist = allWaitlists.get(index);
         if (waitlist.size() > 0) {
             return true;
-        } else {
+        } else if{
             return false;
         }
     }
