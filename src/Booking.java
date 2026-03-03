4d6b40c4e3c519d3eeb75812f6971ce78138909b
Lucas <lucas.singh06@outlook.com>
2026-03-03 11:09:21 -0500

    Corrected minor issues for booking.java. Uses fields to connect to User and Event classes

--- src/Booking.java
+++ src/Booking.java
@@ -1,4 +1,33 @@
 public class Booking {
+    private String bookingId;
+    private User user;
+    private Event event;
     public String status;
 
+    public Booking(String bookingId, User user, Event event, String status) {
+        this.bookingId = bookingId;
+        this.user = user;
+        this.event = event;
+        this.status = status;
+    }
+
+    public String getBookingId() {
+        return bookingId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
