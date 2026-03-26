5109dd90969ba32d9e6da96ac1d6847191eb4641
Luke <lukehodder6@gmail.com>
2026-03-26 15:37:01 -0400

    Implemented loading from file

--- src/FileManager.java
+++ src/FileManager.java
@@ -57,4 +57,94 @@ public class FileManager {
             System.out.println("Error saving bookings.");
         }
     }
-}
+
+    public static void loadUsers(UserFunctions userFunctions) {
+        try (BufferedReader br = new BufferedReader(new FileReader("users.csv"))) {
+            br.readLine(); //skip header
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+
+                String id = data[0];
+                String name = data[1];
+                String email = data[2];
+                String type = data[3];
+
+                userFunctions.createUser(id, name, email, type);
+            }
+        } catch (IOException e) {
+            System.out.println("No users file found.");
+        }
+    }
+
+    public static void loadEvents(EventManagement eventManagement) {
+        try (BufferedReader br = new BufferedReader(new FileReader("events.csv"))) {
+            br.readLine(); //skip header
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+
+                String id = data[0];
+                String title = data[1];
+                String dateTime = data[2];
+                String location = data[3];
+                String capacity = data[4];
+                int cap = Integer.parseInt(capacity);
+                String status = data[5];
+                String type = data[6];
+                String workshop = data[7];
+                String seminar = data[8];
+                String concert = data[9];
+
+                Event event;
+                switch (type) {
+                    case ("Workshop"):
+                        event = new Workshop(id, title, dateTime, location, cap, workshop);
+                        event.setStatus(status);
+                        eventManagement.createEvent(event);
+                        break;
+                    case ("Seminar"):
+                        event = new Seminar(id, title, dateTime, location, cap, seminar);
+                        event.setStatus(status);
+                        eventManagement.createEvent(event);
+                        break;
+                    case ("Concert"):
+                        event = new Concert(id, title, dateTime, location, cap, concert);
+                        event.setStatus(status);
+                        eventManagement.createEvent(event);
+                        break;
+                    default:
+                        System.out.println("Invalid event type in data loading.");
+                }
+
+
+            }
+        } catch (IOException e) {
+            System.out.println("Error loading events from file");
+        }
+    }
+
+    public static void loadBookings(BookingManager bookingManager, UserFunctions userFunctions, EventManagement eventManagement){
+        try (BufferedReader br = new BufferedReader(new FileReader("bookings.csv"))) {
+            br.readLine(); //skip header
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+
+                String userId = data[1];
+                String eventId = data[2];
+
+                User user = userFunctions.getUser(userId);
+                Event event = eventManagement.getEvent(eventId);
+
+                bookingManager.createBooking(user, event);
+            }
+        }
+        catch (IOException e){
+            System.out.println("No booking file found.");
+        }
+        }
+    }
